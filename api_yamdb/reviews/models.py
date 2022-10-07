367df328b9bd85e30057f96ec494863c2959bb1e
Denis Smirnov <desm80@yandex.ru>
2022-10-07 22:08:20 +0300

    Второй коммит: Добавлен скелет моделей в review.models, зарегистрированы приложения в settings.py

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,3 +1,27 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    ...
+
+
+class Genre(models.Model):
+    ...
+
+
+class Title(models.Model):
+    ...
+
+
+class Review(models.Model):
+    ...
+
+
+class Comment(models.Model):
+    ...
+
+
+class User(AbstractUser):
+    ...
+
