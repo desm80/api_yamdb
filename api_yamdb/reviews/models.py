3645a9da6ecfa9f9af5691b33fda18a009b4e8c0
Denis Smirnov <desm80@yandex.ru>
2022-10-08 12:06:11 +0300

    Второй коммит: Добавлен скелет моделей в review.models, зарегистрированы приложения в settings.py

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -3,25 +3,25 @@ from django.db import models
 
 
 class Category(models.Model):
-    ...
+    pass
 
 
 class Genre(models.Model):
-    ...
+    pass
 
 
 class Title(models.Model):
-    ...
+    pass
 
 
 class Review(models.Model):
-    ...
+    pass
 
 
 class Comment(models.Model):
-    ...
+    pass
 
 
 class User(AbstractUser):
-    ...
+    pass
 
