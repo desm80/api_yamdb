ad2658dbd0a3fc9a7f955ffdf745dbf654bd8bac
Илья Соболев <i4sobolev@yandex.ru>
2022-10-12 21:56:18 +0300

    models plus carcas in serializers and views

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,5 +1,10 @@
 
+from django.contrib.auth import get_user_model  # пока оставлю так
 from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
+from django.db.models import UniqueConstraint
+
+User = get_user_model()  # пока оставлю так, а то питон подчеркивает
 
 
 class Category(models.Model):
@@ -15,17 +20,42 @@ class Title(models.Model):
 
 
 class Review(models.Model):
-    pass
+    title = models.ForeignKey(Title, on_delete=models.CASCADE,
+                              related_name='reviews')
+    text = models.TextField()
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='reviews')
+    score = models.SmallIntegerField(validators=[
+        MinValueValidator(1),
+        MaxValueValidator(10)
+    ])
+
+    pub_date = models.DateTimeField(
+        'Дата добавления', auto_now_add=True, db_index=True)
+
+    class Meta:
+        ordering = ("-pub_date",)
+        constraints = [
+            UniqueConstraint(
+                fields=['author', 'title'],
+                name='unique_review')
+        ]
+
+    def __str__(self):
+        return self.text
 
 
 class Comment(models.Model):
-    pass
+    review = models.ForeignKey(Review, on_delete=models.CASCADE,
+                               related_name='comments')
+    text = models.TextField()
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='comments')
+    pub_date = models.DateTimeField(
+        'Дата добавления', auto_now_add=True, db_index=True)
 
+    class Meta:
+        ordering = ("-pub_date",)
 
-<<<<<<< HEAD
-
-=======
-# class User(AbstractUser):
-#     pass
->>>>>>> master
-
+    def __str__(self):
+        return self.text
