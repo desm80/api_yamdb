74bd2869b38e9c4eac17b26ae60831c9901e96e2
Denis Smirnov <desm80@yandex.ru>
2022-10-12 07:38:37 +0300

    коммит 8: Добавил обработку урла me

--- api_yamdb/users/views.py
+++ api_yamdb/users/views.py
@@ -5,9 +5,11 @@ from django.conf import settings
 from django.contrib.auth import get_user_model
 from django.core.mail import send_mail
 from rest_framework import viewsets, filters, status
+from rest_framework.decorators import action
 from rest_framework.generics import CreateAPIView
 from rest_framework.pagination import PageNumberPagination
 from djoser.views import UserViewSet
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework_simplejwt.views import TokenViewBase
 
@@ -25,6 +27,20 @@ class UserViewSet(viewsets.ModelViewSet):
     lookup_field = 'username'
     filter_backends = (filters.SearchFilter,)
     search_fields = ('^username',)
+
+    @action(methods=['patch', 'get'],
+            permission_classes=[IsAuthenticated],
+            detail=False)
+    def me(self, request, *args, **kwargs):
+        user = self.request.user
+        serializer = self.get_serializer(user)
+        if self.request.method == 'PATCH':
+            serializer = self.get_serializer(user,
+                                             data=request.data,
+                                             partial=True)
+            serializer.is_valid(raise_exception=True)
+            serializer.save()
+        return Response(serializer.data)
 
 
 class TokenObtainPairView(TokenViewBase):
